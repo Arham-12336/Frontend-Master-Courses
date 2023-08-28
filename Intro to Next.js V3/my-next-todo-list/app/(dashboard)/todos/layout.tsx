7c4c18f8f00593a103057562d3f241b0647da123
Muhammad Arham <muhammad.arham1019@gmail.com>
2023-08-28 23:51:21 +0500

    Removed unnecessary files

---
+++ Intro to Next.js V3/my-next-todo-list/app/(dashboard)/todos/layout.tsx
@@ -0,0 +1,9 @@
+const DashboardLayout=({children})=>{
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <div>{children}</div>
+        </div>
+    );
+};
+export default DashboardLayout;
