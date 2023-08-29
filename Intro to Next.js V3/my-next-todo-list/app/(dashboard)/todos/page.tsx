a54698a5c627be5379b7d1be221faac8a215e923
Muhammad Arham <muhammad.arham1019@gmail.com>
2023-08-29 22:22:51 +0500

    Fetching data with prisma

--- Intro to Next.js V3/my-next-todo-list/app/(dashboard)/todos/page.tsx
+++ Intro to Next.js V3/my-next-todo-list/app/(dashboard)/todos/page.tsx
@@ -1,6 +1,17 @@
-console.log(process.argv)
+import TodoList from '@/components/TodoList';
+import db from '@/utils/db'
 
-const TodosPage=()=>{
-    return <h1>TODOS PAGE</h1>
+const getData=async()=>{
+const todos=await db.todo.findMany({});
+return todos;
+}
+
+const TodosPage=async()=>{
+    const todos=await getData();
+    return(
+        <div>
+            <TodoList todos={todos}/>
+        </div>
+    )
 }
 export default TodosPage;
---
+++ Intro to Next.js V3/my-next-todo-list/components/Todo.tsx
@@ -0,0 +1,6 @@
+const Todo=({todo})=>{
+    return <div>
+        {todo.content}
+    </div>
+};
+export default Todo;
