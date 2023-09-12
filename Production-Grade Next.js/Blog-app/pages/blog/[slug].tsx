da59374f2a992fe0b28d65d09abcbff24c648f12
Muhammad Arham <muhammad.arham1019@gmail.com>
2023-09-13 00:31:04 +0500

    Started working with dynamic paths with static paths

--- Production-Grade Next.js/Blog-app/pages/blog/[slug].tsx
+++ Production-Grade Next.js/Blog-app/pages/blog/[slug].tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react'
 import hydrate from 'next-mdx-remote/hydrate'
+import matter from 'gray-matter'
 import { majorScale, Pane, Heading, Spinner } from 'evergreen-ui'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Post } from '../../types'
 import Container from '../../components/container'
 import HomeNav from '../../components/homeNav'
+import fs from 'fs';
+import path from 'path'
 
 const BlogPost: FC<Post> = ({ source, frontMatter }) => {
   const content = hydrate(source)
@@ -42,6 +45,24 @@ const BlogPost: FC<Post> = ({ source, frontMatter }) => {
 BlogPost.defaultProps = {
   source: '',
   frontMatter: { title: 'default title', summary: 'summary', publishedOn: '' },
+}
+
+export function getStaticPaths(){
+  const postPath =path.join(process.cwd(),'posts');
+  const filenames=fs.readdirSync(postPath)
+  const slugs=filenames.map(name=>{
+    const filepath=path.join(postPath,name)
+    const file=fs.readFileSync(filepath,'utf-8')
+    const {data}=matter(file)
+    return data
+  })
+
+  return{
+    paths:slugs.map(s=>
+      {
+        params:{slug:s.slug}
+      })
+  }
 }
 
 /**
