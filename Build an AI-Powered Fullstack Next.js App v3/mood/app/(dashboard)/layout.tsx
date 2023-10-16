6c6605b295b2c7372b1160f101d74b86dcd8e96c
Muhammad Arham <muhammad.arham1019@gmail.com>
2023-10-16 23:06:56 +0500

    User profile widget added

--- Build an AI-Powered Fullstack Next.js App v3/mood/app/(dashboard)/layout.tsx
+++ Build an AI-Powered Fullstack Next.js App v3/mood/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen relative">
@@ -5,7 +7,11 @@ const DashboardLayout = ({ children }) => {
         Mood
       </aside>
       <div className="ml-[200px]">
-        <header className="h-[60px] border-b border-black/10">Hello</header>
+        <header className="h-[60px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
         <div>{children}</div>
       </div>
     </div>
