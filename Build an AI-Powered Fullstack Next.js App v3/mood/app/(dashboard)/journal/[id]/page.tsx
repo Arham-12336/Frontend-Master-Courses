1fdc14278da6041b9166cfefbb34b95ac84a9975
Muhammad Arham <muhammad.arham1019@gmail.com>
2023-11-02 21:04:07 +0500

    Added the desiplaying new entry page

---
+++ Build an AI-Powered Fullstack Next.js App v3/mood/app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,27 @@
+import Editor from "@/components/Editor";
+import { getUserByClerkID } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+
+const getEnrty = async (id) => {
+  const user = await getUserByClerkID();
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  });
+
+  return entry;
+};
+
+const EntryPage = async ({ params }) => {
+  const enrty = await getEnrty(params.id);
+  return (
+    <div>
+      <Editor entry={enrty} />
+    </div>
+  );
+};
+export default EntryPage;
