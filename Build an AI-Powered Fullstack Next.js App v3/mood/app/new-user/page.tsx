749be198fce49a615084a11dbb033e69a29ccca7
Muhammad Arham <muhammad.arham1019@gmail.com>
2024-08-14 17:12:03 +0500

    updated the logic

--- Build an AI-Powered Fullstack Next.js App v3/mood/app/new-user/page.tsx
+++ Build an AI-Powered Fullstack Next.js App v3/mood/app/new-user/page.tsx
@@ -3,14 +3,18 @@ import { auth, currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const createNewUser = async () => {
+  let match;
   const user = await currentUser();
   console.log("User : ", user);
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  });
-  if (!match) {
+  if (user) {
+    match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id as string,
+      },
+    });
+  }
+
+  if (!match && user) {
     await prisma.user.create({
       data: {
         clerkId: user.id,
